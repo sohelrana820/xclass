fed900dbba97df36ef7736a265421e08df87e1b8
sohelrana820 <runwithlight820>
2016-08-07 01:44:35 +0600

    Colorr picker option overriding

--- plugins/Apps/webroot/js/src/app.js
+++ plugins/Apps/webroot/js/src/app.js
@@ -1,5 +1,15 @@
 var app = angular.module('Application', ['ngResource', 'textAngular', 'color.picker']);
 
+app.config(function($provide) {
+    $provide.decorator('ColorPickerOptions', function($delegate) {
+        var options = angular.copy($delegate);
+        options.round = true;
+        options.alpha = false;
+        options.format = 'hex';
+        return options;
+    });
+});
+
 app.controller('MainsCtrl', function($scope, LabelResources){
 
 /*    // Getting all active labels.
