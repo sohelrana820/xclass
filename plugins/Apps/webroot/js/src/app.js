e84a3e37ddc2183368f35e9f5e75a4be8ff1fb75
sohelrana820 <runwithlight820>
2016-08-06 18:30:54 +0600

    File moved

--- plugins/Apps/webroot/js/src/app.js
+++ plugins/Apps/webroot/js/src/app.js
@@ -3,9 +3,13 @@ var app = angular.module('Application', ['ngResource', 'textAngular']);
 app.controller('MainsCtrl', function($scope, LabelResources){
 
     // Getting all active labels.
-    $scope.labels = [];
-    var labels = LabelResources.query().$promise;
-    labels.then(function (res) {
-        $scope.labels = res.labels
-    });
+    $scope.fetchLabelsLists = function(){
+        $scope.labels = [];
+        var labels = LabelResources.query().$promise;
+        labels.then(function (res) {
+            $scope.labels = res.labels
+        });
+    };
+
+    $scope.fetchLabelsLists();
 });
