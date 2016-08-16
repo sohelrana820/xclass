703f3b83a40fe8988b7930d8ef024141511da4f2
sohelrana820 <runwithlight820>
2016-08-17 05:25:52 +0600

    Create database through code is working fine!

--- src/Controller/InstallationController.php
+++ src/Controller/InstallationController.php
@@ -9,7 +9,10 @@
 namespace App\Controller;
 
 
+use Cake\Core\Exception\Exception;
+use Cake\Datasource\ConnectionManager;
 use Cake\Event\Event;
+use Cake\Filesystem\File;
 
 class InstallationController extends AppController{
 
@@ -33,7 +36,28 @@ class InstallationController extends AppController{
 
     public function database()
     {
+        $dbConf = [
+            'host' => 'localhost',
+            'username' => 'root',
+            'password' => 'ltqpsmr7',
+            'database' => 'task_manager3',
+        ];
+        try {
+            $dsn = 'mysql://'.$dbConf['username'].':'.$dbConf['password'].'@'.$dbConf['host'].'/';
+            ConnectionManager::config('create_database', ['url' => $dsn]);
+            $connection = ConnectionManager::get('create_database');
+            $connection->query('CREATE DATABASE IF NOT EXISTS '.$dbConf['database']);
 
+            $dsn = 'mysql://'.$dbConf['username'].':'.$dbConf['password'].'@'.$dbConf['host'].'/'.$dbConf['database'].'';
+            ConnectionManager::config('application_database', ['url' => $dsn]);
+            $connection = ConnectionManager::get('application_database');
+
+            $structureSql = new File(CONFIG.'schema/structure.sql');
+            $connection->query($structureSql->read());
+        }
+        catch(Exception $e){
+            var_dump($e->getMessage());
+        }
     }
 
     public function general()
