ebd55b2fbe4f49e02d0969c8504e9df3858585ea
sohelrana820 <runwithlight820>
2016-08-18 00:52:33 +0600

    Remove angular

--- plugins/Apps/webroot/js/src/installation.js
+++
@@ -1,9 +0,0 @@
-var app = angular.module('Application', []);
-
-app.controller('Requirements', function($scope, $timeout){
-
-    /*$timeout(function() {
-        window.location.href = "/tasks";
-    }, 1000);*/
-
-});
