3769e923eef202e27b4a61a452c4bbc597a1410d
sohelrana820 <me.sohelrana@gmail.com>
2016-08-19 16:11:59 +0600

    New DB

--- config/schema/structure.sql
+++ config/schema/structure.sql
@@ -1,17 +1,17 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.10deb1
+-- version 4.5.4.1deb2ubuntu2
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Aug 14, 2016 at 04:37 AM
--- Server version: 5.5.49-0ubuntu0.14.04.1
--- PHP Version: 5.5.9-1ubuntu4.17
+-- Generation Time: Aug 19, 2016 at 04:11 PM
+-- Server version: 5.7.13-0ubuntu0.16.04.2
+-- PHP Version: 7.0.8-0ubuntu0.16.04.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 --
--- Database: `task-manager`
+-- Database: `my_personal_tacker`
 --
 
 -- --------------------------------------------------------
@@ -20,18 +20,15 @@ SET time_zone = "+00:00";
 -- Table structure for table `attachments`
 --
 
-CREATE TABLE IF NOT EXISTS `attachments` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `uuid` varchar(32) NOT NULL,
+CREATE TABLE `attachments` (
+  `id` int(11) NOT NULL,
+  `uuid` varchar(36) NOT NULL,
   `task_id` int(11) DEFAULT NULL,
   `comment_id` int(11) DEFAULT NULL,
   `name` varchar(255) NOT NULL,
   `path` varchar(255) NOT NULL,
-  `created` datetime NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `task_id` (`task_id`),
-  KEY `comment_id` (`comment_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+  `created` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -39,19 +36,16 @@ CREATE TABLE IF NOT EXISTS `attachments` (
 -- Table structure for table `comments`
 --
 
-CREATE TABLE IF NOT EXISTS `comments` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `comments` (
+  `id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `task_id` int(11) NOT NULL,
   `comment` text,
   `status` int(1) NOT NULL DEFAULT '1' COMMENT 'status: 1 = active, 2 = Inactive',
   `created` datetime NOT NULL,
   `modified` datetime NOT NULL,
-  `changing_status` varchar(12) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `user_id` (`user_id`),
-  KEY `comment_id` (`task_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=102 ;
+  `changing_status` varchar(12) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -59,17 +53,15 @@ CREATE TABLE IF NOT EXISTS `comments` (
 -- Table structure for table `labels`
 --
 
-CREATE TABLE IF NOT EXISTS `labels` (
-  `id` int(3) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `labels` (
+  `id` int(3) NOT NULL,
   `name` varchar(128) NOT NULL,
   `color_code` varchar(24) DEFAULT 'eeeeee',
   `created_by` int(11) NOT NULL,
   `status` int(1) NOT NULL DEFAULT '1' COMMENT 'status: 1 = active,  = Inactive',
   `created` datetime NOT NULL,
-  `modified` datetime NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `created_by` (`created_by`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=12 ;
+  `modified` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -77,8 +69,8 @@ CREATE TABLE IF NOT EXISTS `labels` (
 -- Table structure for table `profiles`
 --
 
-CREATE TABLE IF NOT EXISTS `profiles` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `profiles` (
+  `id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `created_by` int(11) DEFAULT NULL,
   `first_name` varchar(32) NOT NULL,
@@ -94,12 +86,8 @@ CREATE TABLE IF NOT EXISTS `profiles` (
   `postal_code` varchar(8) DEFAULT NULL,
   `country` varchar(32) DEFAULT NULL,
   `created` datetime NOT NULL,
-  `modified` datetime NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `user_id` (`user_id`),
-  KEY `created_by` (`created_by`),
-  KEY `user_id_2` (`user_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=22 ;
+  `modified` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -107,18 +95,16 @@ CREATE TABLE IF NOT EXISTS `profiles` (
 -- Table structure for table `tasks`
 --
 
-CREATE TABLE IF NOT EXISTS `tasks` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `uuid` varchar(32) NOT NULL,
+CREATE TABLE `tasks` (
+  `id` int(11) NOT NULL,
+  `uuid` varchar(36) NOT NULL,
   `created_by` int(11) NOT NULL,
   `task` text NOT NULL,
   `description` text,
   `status` int(1) NOT NULL DEFAULT '1' COMMENT 'status: 1 = open,  2 = closed, 3 = reopen',
   `modified` datetime NOT NULL,
-  `created` datetime NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `created_by` (`created_by`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=11 ;
+  `created` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -126,17 +112,13 @@ CREATE TABLE IF NOT EXISTS `tasks` (
 -- Table structure for table `tasks_labels`
 --
 
-CREATE TABLE IF NOT EXISTS `tasks_labels` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `tasks_labels` (
+  `id` int(11) NOT NULL,
   `task_id` int(11) NOT NULL,
   `label_id` int(11) NOT NULL,
   `created` datetime NOT NULL,
-  `status` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `task_id` (`task_id`),
-  KEY `label_id` (`label_id`),
-  KEY `label_id_2` (`label_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=38 ;
+  `status` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -144,20 +126,19 @@ CREATE TABLE IF NOT EXISTS `tasks_labels` (
 -- Table structure for table `users`
 --
 
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `uuid` varchar(32) NOT NULL,
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `uuid` varchar(36) NOT NULL,
   `username` varchar(64) NOT NULL,
   `password` varchar(128) NOT NULL COMMENT 'default password<123456>: $2y$10$QRvZkbtezmQ9ujWFpgqTeOJ3mqbyt78CAIUqAoB3Us1XlmDWH4j2a',
   `role` int(1) NOT NULL DEFAULT '2' COMMENT 'role: 1 = admin, 2 = user',
   `status` int(1) NOT NULL DEFAULT '1' COMMENT 'status: 1 = Active, 0 = Inactive',
-  `email_verifying_code` varchar(32) NOT NULL,
+  `email_verifying_code` varchar(32) DEFAULT NULL,
   `forgot_pass_code` varchar(32) DEFAULT NULL,
   `email_verify` int(1) NOT NULL DEFAULT '0' COMMENT 'email_verify: 1 = Verified, 0 =  Unverified',
   `created` datetime NOT NULL,
-  `modified` datetime NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=22 ;
+  `modified` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -165,17 +146,124 @@ CREATE TABLE IF NOT EXISTS `users` (
 -- Table structure for table `users_tasks`
 --
 
-CREATE TABLE IF NOT EXISTS `users_tasks` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `users_tasks` (
+  `id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `task_id` int(11) NOT NULL,
   `created` datetime NOT NULL,
-  `modified` datetime NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `user_id` (`user_id`,`task_id`),
-  KEY `task_id` (`task_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=40 ;
+  `modified` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `attachments`
+--
+ALTER TABLE `attachments`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `task_id` (`task_id`),
+  ADD KEY `comment_id` (`comment_id`);
+
+--
+-- Indexes for table `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `comment_id` (`task_id`);
+
+--
+-- Indexes for table `labels`
+--
+ALTER TABLE `labels`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `created_by` (`created_by`);
+
+--
+-- Indexes for table `profiles`
+--
+ALTER TABLE `profiles`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `created_by` (`created_by`),
+  ADD KEY `user_id_2` (`user_id`);
+
+--
+-- Indexes for table `tasks`
+--
+ALTER TABLE `tasks`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `created_by` (`created_by`);
+
+--
+-- Indexes for table `tasks_labels`
+--
+ALTER TABLE `tasks_labels`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `task_id` (`task_id`),
+  ADD KEY `label_id` (`label_id`),
+  ADD KEY `label_id_2` (`label_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users_tasks`
+--
+ALTER TABLE `users_tasks`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`,`task_id`),
+  ADD KEY `task_id` (`task_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `attachments`
+--
+ALTER TABLE `attachments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=102;
+--
+-- AUTO_INCREMENT for table `labels`
+--
+ALTER TABLE `labels`
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+--
+-- AUTO_INCREMENT for table `profiles`
+--
+ALTER TABLE `profiles`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+--
+-- AUTO_INCREMENT for table `tasks`
+--
+ALTER TABLE `tasks`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+--
+-- AUTO_INCREMENT for table `tasks_labels`
+--
+ALTER TABLE `tasks_labels`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+--
+-- AUTO_INCREMENT for table `users_tasks`
+--
+ALTER TABLE `users_tasks`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
 --
 -- Constraints for dumped tables
 --
