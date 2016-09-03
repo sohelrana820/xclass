1b177d05c5d8868b39106ab210ec9d419de7e085
sohelrana820 <runwithlight820>
2016-09-03 08:21:32 +0600

    taslk list done

--- plugins/Apps/webroot/js/custom.js
+++ plugins/Apps/webroot/js/custom.js
@@ -1,25 +1,3 @@
-$('.task_operation').on('click', function (event) {
-    $(this).parent().find('.dropdown').toggleClass('open');
-});
+$( ".dropdown-menu" ).mouseover(function() {
 
-$('.close_dropdown').on('click', function (event) {
-    $(this).parent().parent().parent().toggleClass('open');
-    console.log(1111);
-});
-
-
-$('.close_dropdown').on('mouseleave', function (event) {
-    console.log(11);
-});
-
-
-$('ul.nav li.dropdown').hover(function() {
-    $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
-}, function() {
-    $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
-});
-
-$('body').on('click', 'a.disabled', function(event) {
-    console.log(111);
-    event.preventDefault();
 });
