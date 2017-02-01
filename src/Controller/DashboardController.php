217d646aa2810c472919bc9424a33fa5f5e6fab6
sohelrana820 <me.sohelrana@gmail.com>
2017-02-02 03:00:49 +0600

    Clean up some files and codes

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -23,16 +23,4 @@ class DashboardController extends AppController{
         }
         $this->set('projects', $projects);
     }
-
-    public function overview()
-    {
-        $overview = [
-            'total_user' => $this->Users->countTotalUser(),
-            'total_label' => $this->Labels->countTotalLabel(),
-            'total_open_task' => $this->Tasks->countTotalTasks($stauts = 1),
-            'total_closed_task' => $this->Tasks->countTotalTasks($stauts = 2)
-        ];
-        $this->set('result', $overview);
-        $this->set('_serialize', ['result']);
-    }
 } 
