a24f579fc4872a2631551663f0160edcef73e405
sohelrana820 <me.sohelrana@gmail.com>
2017-02-07 01:49:04 +0600

    Comment is now tracking...

--- plugins/Apps/webroot/js/custom.js
+++ plugins/Apps/webroot/js/custom.js
@@ -8,6 +8,7 @@ var contentPosition = (loaderHeight - loaderHContentHeight) / 2;
 $('.loader_content').css('top', contentPosition);
 
 var contentHeight = $('.content-area').height() + 50;
-console.log(contentHeight);
+var sideBAr = $('#sidebar-wrapper').height() + 50;
+console.log(sideBAr);
 console.log(11);
 $('#page-content-wrapper').css('height', contentHeight);
--- plugins/Apps/webroot/js/theme.js
+++ plugins/Apps/webroot/js/theme.js
@@ -2,3 +2,8 @@ $("#menu-toggle").click(function(e) {
     e.preventDefault();
     $("#wrapper").toggleClass("toggled");
 });
+
+$( window ).load(function() {
+    var sideBarHeight = $('#wrapper').height();
+    $('.right-sidebar').css('height', sideBarHeight);
+});
