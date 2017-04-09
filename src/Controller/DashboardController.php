47dd106c019a55093d7f74bf75a917fdeedb3b31
sohelrana820 <me.sohelrana@gmail.com>
2017-04-10 02:03:06 +0600

    DashboardController not PSR2 compitable

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -1,20 +1,13 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: root
- * Date: 9/21/15
- * Time: 3:38 AM
- */
 
 namespace App\Controller;
-
 
 /**
  * Class DashboardController
  * @package App\Controller
  */
-class DashboardController extends AppController{
-
+class DashboardController extends AppController
+{
     /**
      * @var string
      */
@@ -23,14 +16,15 @@ class DashboardController extends AppController{
     /**
      * Dashboard content
      */
-    public function index(){
+    public function index()
+    {
         $this->loadModel('Projects');
         $projects = $this->Projects->getProjectsForDashboard($this->loggedInUser, 7);
-        foreach ($projects as $key => $project)
-        {
+        foreach ($projects as $key => $project) {
             $overview = $this->getProjectOverview($project->id);
             $projects[$key]['overview'] = $overview;
         }
+
         $this->set('projects', $projects);
     }
-} 
+}
