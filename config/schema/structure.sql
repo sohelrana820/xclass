08816a4b9016c0026fa0185270f4c7996cc305e5
sohelrana820 <me.sohelrana@gmail.com>
2018-03-24 04:13:22 +0600

    took db structure

--- config/schema/structure.sql
+++ config/schema/structure.sql
@@ -3,80 +3,75 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Mar 11, 2017 at 12:32 AM
--- Server version: 5.7.17-0ubuntu0.16.04.1
--- PHP Version: 7.0.15-0ubuntu0.16.04.4
+-- Generation Time: Mar 23, 2018 at 10:12 PM
+-- Server version: 5.7.21-0ubuntu0.16.04.1
+-- PHP Version: 7.0.28-0ubuntu0.16.04.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 --
--- Database: `task_manager`
+-- Database: `xclass`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `attachments`
+-- Table structure for table `courses`
 --
 
-CREATE TABLE `attachments` (
-  `id` int(11) NOT NULL,
-  `uuid` varchar(36) NOT NULL,
-  `project_id` int(11) DEFAULT NULL,
-  `task_id` int(11) DEFAULT NULL,
-  `comment_id` int(11) DEFAULT NULL,
+CREATE TABLE `courses` (
+  `id` int(4) NOT NULL,
   `name` varchar(255) NOT NULL,
-  `path` varchar(255) NOT NULL,
-  `created` datetime NOT NULL
+  `description` text NOT NULL,
+  `status` int(1) NOT NULL DEFAULT '1' COMMENT 'status: 1 = active, 0 = inactive',
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `comments`
+-- Table structure for table `courses_users`
 --
 
-CREATE TABLE `comments` (
+CREATE TABLE `courses_users` (
+  `id` int(11) NOT NULL,
+  `cource_id` int(11) NOT NULL,
+  `user_id` int(3) NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `documents`
+--
+
+CREATE TABLE `documents` (
   `id` int(11) NOT NULL,
   `uuid` varchar(36) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `task_id` int(11) NOT NULL,
-  `comment` text,
-  `status` int(1) NOT NULL DEFAULT '1' COMMENT 'status: 1 = active, 2 = Inactive',
-  `created` datetime NOT NULL,
-  `modified` datetime NOT NULL,
-  `changing_status` varchar(12) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `feeds`
---
-
-CREATE TABLE `feeds` (
-  `id` int(11) NOT NULL,
-  `project_id` int(11) DEFAULT NULL,
-  `task_id` int(11) DEFAULT NULL,
-  `title` text NOT NULL,
-  `event` varchar(64) NOT NULL,
-  `created` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `labels`
---
-
-CREATE TABLE `labels` (
-  `id` int(3) NOT NULL,
-  `project_id` int(11) NOT NULL,
-  `name` varchar(128) NOT NULL,
-  `color_code` varchar(24) DEFAULT 'eeeeee',
   `created_by` int(11) NOT NULL,
-  `status` int(1) NOT NULL DEFAULT '1' COMMENT 'status: 1 = active,  = Inactive',
+  `title` varchar(255) NOT NULL,
+  `image` varchar(255) DEFAULT NULL,
+  `desctiotion` text,
+  `path` varchar(255) NOT NULL,
+  `status` int(1) NOT NULL DEFAULT '1' COMMENT 'status: 1 = active, 0 = inactive',
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `download`
+--
+
+CREATE TABLE `download` (
+  `id` int(11) NOT NULL,
+  `document_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
   `created` datetime NOT NULL,
   `modified` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -110,81 +105,13 @@ CREATE TABLE `profiles` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `projects`
---
-
-CREATE TABLE `projects` (
-  `id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `name` varchar(128) NOT NULL,
-  `slug` varchar(128) NOT NULL,
-  `description` text,
-  `deadline` date DEFAULT NULL,
-  `status` int(1) NOT NULL DEFAULT '1' COMMENT 'status: 1 = Progressing, 2 = Paused, 3 = Invalid, 4 = Completed',
-  `note` varchar(255) DEFAULT NULL,
-  `created` datetime NOT NULL,
-  `last_opened` datetime DEFAULT NULL,
-  `modified` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `projects_users`
---
-
-CREATE TABLE `projects_users` (
-  `id` int(11) NOT NULL,
-  `project_id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `status` int(1) NOT NULL DEFAULT '1' COMMENT 'status: 1 = active,  0 = Inactive',
-  `created` datetime NOT NULL,
-  `modified` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `settings`
 --
 
 CREATE TABLE `settings` (
   `id` int(11) NOT NULL,
   `meta` varchar(128) NOT NULL,
-  `value` int(255) NOT NULL,
-  `created` datetime NOT NULL,
-  `modified` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tasks`
---
-
-CREATE TABLE `tasks` (
-  `id` int(11) NOT NULL,
-  `identity` int(11) NOT NULL,
-  `uuid` varchar(36) NOT NULL,
-  `project_id` int(11) NOT NULL,
-  `created_by` int(11) NOT NULL,
-  `task` text NOT NULL,
-  `description` text,
-  `status` int(1) NOT NULL DEFAULT '1' COMMENT 'status: 1 = open,  2 = closed, 3 = reopen',
-  `modified` datetime NOT NULL,
-  `created` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tasks_labels`
---
-
-CREATE TABLE `tasks_labels` (
-  `id` int(11) NOT NULL,
-  `task_id` int(11) NOT NULL,
-  `label_id` int(11) NOT NULL,
+  `value` text NOT NULL,
   `created` datetime NOT NULL,
   `modified` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -199,26 +126,13 @@ CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `uuid` varchar(36) NOT NULL,
   `username` varchar(64) NOT NULL,
+  `student_id` varchar(32) DEFAULT NULL,
   `password` varchar(128) NOT NULL COMMENT 'default password<123456>: $2y$10$QRvZkbtezmQ9ujWFpgqTeOJ3mqbyt78CAIUqAoB3Us1XlmDWH4j2a',
-  `role` int(1) NOT NULL DEFAULT '2' COMMENT 'role: 1 = admin, 2 = user',
-  `status` int(1) NOT NULL DEFAULT '1' COMMENT 'status: 1 = Active, 0 = Inactive',
+  `role` int(1) NOT NULL DEFAULT '2' COMMENT 'role: 1 = admin, 2 = student',
+  `status` int(1) NOT NULL DEFAULT '0' COMMENT 'status: 1 = Active, 0 = Inactive',
   `email_verifying_code` varchar(32) DEFAULT NULL,
   `forgot_pass_code` varchar(32) DEFAULT NULL,
   `email_verify` int(1) NOT NULL DEFAULT '0' COMMENT 'email_verify: 1 = Verified, 0 =  Unverified',
-  `created` datetime NOT NULL,
-  `modified` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users_tasks`
---
-
-CREATE TABLE `users_tasks` (
-  `id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `task_id` int(11) NOT NULL,
   `created` datetime NOT NULL,
   `modified` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -228,37 +142,34 @@ CREATE TABLE `users_tasks` (
 --
 
 --
--- Indexes for table `attachments`
+-- Indexes for table `courses`
 --
-ALTER TABLE `attachments`
+ALTER TABLE `courses`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `task_id` (`task_id`),
-  ADD KEY `comment_id` (`comment_id`),
-  ADD KEY `project_id` (`project_id`);
+  ADD KEY `name` (`name`);
 
 --
--- Indexes for table `comments`
+-- Indexes for table `courses_users`
 --
-ALTER TABLE `comments`
+ALTER TABLE `courses_users`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `user_id` (`user_id`),
-  ADD KEY `comment_id` (`task_id`);
+  ADD KEY `cource_id` (`cource_id`),
+  ADD KEY `user_id` (`user_id`);
 
 --
--- Indexes for table `feeds`
+-- Indexes for table `documents`
 --
-ALTER TABLE `feeds`
+ALTER TABLE `documents`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `project_id` (`project_id`),
-  ADD KEY `task_id` (`task_id`);
+  ADD KEY `created_by` (`created_by`);
 
 --
--- Indexes for table `labels`
+-- Indexes for table `download`
 --
-ALTER TABLE `labels`
+ALTER TABLE `download`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `created_by` (`created_by`),
-  ADD KEY `project_id` (`project_id`);
+  ADD KEY `document_id` (`document_id`),
+  ADD KEY `user_id` (`user_id`);
 
 --
 -- Indexes for table `profiles`
@@ -270,45 +181,10 @@ ALTER TABLE `profiles`
   ADD KEY `user_id_2` (`user_id`);
 
 --
--- Indexes for table `projects`
---
-ALTER TABLE `projects`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `slug` (`slug`),
-  ADD KEY `user_id` (`user_id`);
-
---
--- Indexes for table `projects_users`
---
-ALTER TABLE `projects_users`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `project_id` (`project_id`),
-  ADD KEY `user_id` (`user_id`),
-  ADD KEY `project_id_2` (`project_id`),
-  ADD KEY `user_id_2` (`user_id`);
-
---
 -- Indexes for table `settings`
 --
 ALTER TABLE `settings`
   ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `tasks`
---
-ALTER TABLE `tasks`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `created_by` (`created_by`),
-  ADD KEY `project_id` (`project_id`);
-
---
--- Indexes for table `tasks_labels`
---
-ALTER TABLE `tasks_labels`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `task_id` (`task_id`),
-  ADD KEY `label_id` (`label_id`),
-  ADD KEY `label_id_2` (`label_id`);
 
 --
 -- Indexes for table `users`
@@ -317,108 +193,67 @@ ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `users_tasks`
---
-ALTER TABLE `users_tasks`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `user_id` (`user_id`,`task_id`),
-  ADD KEY `task_id` (`task_id`);
-
---
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `attachments`
+-- AUTO_INCREMENT for table `courses`
 --
-ALTER TABLE `attachments`
+ALTER TABLE `courses`
+  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `courses_users`
+--
+ALTER TABLE `courses_users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
--- AUTO_INCREMENT for table `comments`
+-- AUTO_INCREMENT for table `documents`
 --
-ALTER TABLE `comments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=64;
+ALTER TABLE `documents`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
--- AUTO_INCREMENT for table `feeds`
+-- AUTO_INCREMENT for table `download`
 --
-ALTER TABLE `feeds`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=199;
---
--- AUTO_INCREMENT for table `labels`
---
-ALTER TABLE `labels`
-  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+ALTER TABLE `download`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `profiles`
 --
 ALTER TABLE `profiles`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
---
--- AUTO_INCREMENT for table `projects`
---
-ALTER TABLE `projects`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
---
--- AUTO_INCREMENT for table `projects_users`
---
-ALTER TABLE `projects_users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 --
 -- AUTO_INCREMENT for table `settings`
 --
 ALTER TABLE `settings`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 --
--- AUTO_INCREMENT for table `tasks`
---
-ALTER TABLE `tasks`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
---
--- AUTO_INCREMENT for table `tasks_labels`
---
-ALTER TABLE `tasks_labels`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
---
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
---
--- AUTO_INCREMENT for table `users_tasks`
---
-ALTER TABLE `users_tasks`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 --
 -- Constraints for dumped tables
 --
 
 --
--- Constraints for table `attachments`
+-- Constraints for table `courses_users`
 --
-ALTER TABLE `attachments`
-  ADD CONSTRAINT `attachments_ibfk_1` FOREIGN KEY (`task_id`) REFERENCES `tasks` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `attachments_ibfk_2` FOREIGN KEY (`comment_id`) REFERENCES `comments` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `attachments_ibfk_3` FOREIGN KEY (`project_id`) REFERENCES `projects` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `courses_users`
+  ADD CONSTRAINT `course_student` FOREIGN KEY (`cource_id`) REFERENCES `courses` (`id`),
+  ADD CONSTRAINT `student_course` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 
 --
--- Constraints for table `comments`
+-- Constraints for table `documents`
 --
-ALTER TABLE `comments`
-  ADD CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
-  ADD CONSTRAINT `comments_ibfk_2` FOREIGN KEY (`task_id`) REFERENCES `tasks` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `documents`
+  ADD CONSTRAINT `document_created` FOREIGN KEY (`created_by`) REFERENCES `users` (`id`);
 
 --
--- Constraints for table `feeds`
+-- Constraints for table `download`
 --
-ALTER TABLE `feeds`
-  ADD CONSTRAINT `feeds_ibfk_1` FOREIGN KEY (`project_id`) REFERENCES `projects` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `feeds_ibfk_2` FOREIGN KEY (`task_id`) REFERENCES `tasks` (`id`) ON DELETE SET NULL ON UPDATE SET NULL;
-
---
--- Constraints for table `labels`
---
-ALTER TABLE `labels`
-  ADD CONSTRAINT `labels_ibfk_1` FOREIGN KEY (`project_id`) REFERENCES `projects` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `download`
+  ADD CONSTRAINT `download_docuement` FOREIGN KEY (`document_id`) REFERENCES `documents` (`id`),
+  ADD CONSTRAINT `download_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 
 --
 -- Constraints for table `profiles`
@@ -426,24 +261,3 @@ ALTER TABLE `labels`
 ALTER TABLE `profiles`
   ADD CONSTRAINT `profiles_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `profiles_ibfk_2` FOREIGN KEY (`created_by`) REFERENCES `users` (`id`) ON DELETE SET NULL ON UPDATE SET NULL;
-
---
--- Constraints for table `tasks`
---
-ALTER TABLE `tasks`
-  ADD CONSTRAINT `tasks_ibfk_1` FOREIGN KEY (`created_by`) REFERENCES `users` (`id`),
-  ADD CONSTRAINT `tasks_ibfk_2` FOREIGN KEY (`project_id`) REFERENCES `projects` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `tasks_labels`
---
-ALTER TABLE `tasks_labels`
-  ADD CONSTRAINT `tasks_labels_ibfk_1` FOREIGN KEY (`task_id`) REFERENCES `tasks` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `tasks_labels_ibfk_2` FOREIGN KEY (`label_id`) REFERENCES `labels` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `users_tasks`
---
-ALTER TABLE `users_tasks`
-  ADD CONSTRAINT `users_tasks_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `users_tasks_ibfk_2` FOREIGN KEY (`task_id`) REFERENCES `tasks` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
